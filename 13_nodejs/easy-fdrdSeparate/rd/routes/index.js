f03208c625ea5b3664562c3ea6fd0b5009bfa865
liuchang <1746571816@qq.com>
2021-02-27 23:08:17 +0800

    test

--- 13_nodejs/easy-fdrdSeparate/rd/routes/index.js
+++ 13_nodejs/easy-fdrdSeparate/rd/routes/index.js
@@ -6,6 +6,14 @@ router.get('/', async (ctx, next) => {
   })
 })
 
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
 
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
 
 module.exports = router
