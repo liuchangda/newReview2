35de639ee44ae84bba494dd901c0630526f844af
liuchang <1746571816@qq.com>
2021-02-27 21:55:43 +0800

    前后端分离过程

--- 15_前后端分离/test-rd/routes/index.js
+++ 15_前后端分离/test-rd/routes/index.js
@@ -6,14 +6,6 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
 
 module.exports = router
